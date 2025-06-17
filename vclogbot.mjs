5c87778d18d0fc17c7da9b317badbb4a8340a069
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-17 07:31:21 -0400

    Update vclogbot.mjs

--- vclogbot.mjs
+++ vclogbot.mjs
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 import 'dotenv/config';
-import { log, fs, path, registerHandlers, registerSignals } from '@purinton/common';
 import { createDb } from '@purinton/mysql';
 import { createDiscord } from '@purinton/discord';
+import { log, fs, path, registerHandlers, registerSignals } from '@purinton/common';
+
+registerHandlers({ log });
+registerSignals({ log });
+const packageJson = JSON.parse(fs.readFileSync(path(import.meta, 'package.json')), 'utf8');
+const version = packageJson.version;
+const presence = { activities: [{ name: `🎧 Leveling v${version}`, type: 4 }], status: 'online' };
 
 (async () => {
-    registerHandlers({ log });
-    registerSignals({ log });
-    const packageJson = JSON.parse(fs.readFileSync(path(import.meta, 'package.json')), 'utf8');
-    const version = packageJson.version;
-    const name = `🎧 Leveling v${version}`;
-    const status = 'online';
-    const db = await createDb();
-    registerSignals({ shutdownHook: async () => { await db.end() } });
+    const db = await createDb({ log });
+    registerSignals({ shutdownHook: () => db.end() });
     const client = await createDiscord({
         log,
         rootDir: path(import.meta),
+        context: { db, presence, version },
         intents: {
             Guilds: true,
             GuildMessages: true,
+            MessageContent: false,
+            GuildMembers: false,
+            GuildPresences: false,
             GuildVoiceStates: true,
-        },
-        context: {
-            db,
-            presence: { activities: [{ name, type: 4 }], status },
-        },
+        }
     });
-    registerSignals({ shutdownHook: async () => { await client.destroy() } });
+    registerSignals({ shutdownHook: () => client.destroy() });
 })();
